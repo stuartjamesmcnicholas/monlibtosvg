2ab76f59548d131f87e9955be93e038a697c1629
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-10-17 11:12:45 +0100

    Lighter weight, chunked html output.

--- build_html.py
+++ build_html.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import itertools
 
 dirs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
@@ -24,19 +25,34 @@ tTail = """  </body>
 for d in dirs:
     os.chdir(d)
 
-    t = tHeader
+    svgs = sorted(glob.glob("*.svg"))
+    batches = itertools.batched(svgs,100)
+    ibatch = 1
+    tMain = tHeader
+    tMain += '<ul>\n'
+    for b in batches:
+        t = tHeader
+        for s in b:
+            t += '<figure>\n'
+            t += '<img src="'+s+'" alt="'+s+'" />\n'
+            t += '<figcaption>'+s+'</figcaption>\n'
+            t += '</figure>\n'
 
-    svgs = glob.glob("*.svg")
-    for s in svgs:
-        t += '<figure>'
-        t += '<img src="'+s+'" alt="'+s+'" />\n'
-        t += '<figcaption>'+s+'</figcaption>'
-        t += '</figure>'
+        t += tTail
 
-    t += tTail
+        with open("index-"+str(ibatch)+".html","w+") as f:
+            f.write(t)
 
+        indexLabel = b[0].rstrip(".svg")+'-'+b[-1].rstrip(".svg")
+
+        tMain += '<li><a href="'+'index-'+str(ibatch)+'.html">'+indexLabel+'</a></li>'+'\n'
+
+        ibatch += 1
+
+    tMain += '</ul>\n'
+    tMain += tTail
     with open("index.html","w+") as f:
-        f.write(t)
+        f.write(tMain)
 
     os.chdir("..")
 
